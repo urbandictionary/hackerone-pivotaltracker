320e41d6944e1fb5680828a6374a092d67d4732e
apeckham <apeckham@gmail.com>
2016-11-27 14:08:14 -0800

    Get PORT from env var

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]]
+                 [http-kit "2.2.0"]
+                 [environ "1.1.0"]]
   :main ^:skip-aot hackerone-pivotaltracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -1,12 +1,18 @@
 (ns hackerone-pivotaltracker.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]))
+  (:require [org.httpkit.server :as server]
+            [environ.core :refer [env]]))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
 
+(defn port-env []
+  (if-let [e (env :port)]
+    (Integer/parseInt e)
+    8080))
+
 (defn -main
   [& args]
-  (server/run-server app {:port 8080}))
+  (server/run-server app {:port (port-env)}))

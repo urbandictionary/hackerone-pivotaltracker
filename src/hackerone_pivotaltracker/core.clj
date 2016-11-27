5685392edf9671c25fbd2bcb177c53e877f30567
apeckham <apeckham@gmail.com>
2016-11-27 14:05:31 -0800

    Add http-kit server on 8080

--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -1,7 +1,12 @@
 (ns hackerone-pivotaltracker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as server]))
+
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (server/run-server app {:port 8080}))

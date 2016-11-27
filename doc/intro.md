e1bbc455738a91a7db6fd9f8d2a7bb11c282b81e
apeckham <apeckham@gmail.com>
2016-11-27 14:03:19 -0800

    lein new app

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hackerone-pivotaltracker
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hackerone_pivotaltracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns hackerone-pivotaltracker.core-test
+  (:require [clojure.test :refer :all]
+            [hackerone-pivotaltracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

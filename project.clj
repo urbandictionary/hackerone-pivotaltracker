8de9649a5405ff717e925dca0ae7374c61e8063b
apeckham <apeckham@gmail.com>
2016-11-27 15:40:37 -0800

    Add tracker-create to post to pivotaltracker

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [http-kit "2.2.0"]
                  [environ "1.1.0"]
                  [compojure "1.5.1"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.logging "0.3.1"]]
   :min-lein-version "2.0.0"
   :plugins [[lein-environ "0.4.0"]]
--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -1,6 +1,8 @@
 (ns hackerone-pivotaltracker.core
   (:gen-class)
   (:require [org.httpkit.server :as server]
+            [org.httpkit.client :as client]
+            [clojure.data.json :as json]
             [environ.core :refer [env]]
             [clojure.tools.logging :as log]))
 
@@ -8,6 +10,18 @@
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
+
+(defn tracker-post-url []
+  (format "https://www.pivotaltracker.com/services/v5/projects/%s/stories" (env :pivotaltracker-project-id)))
+
+(defn tracker-create [attrs]
+  (let [options {:body    (json/write-str attrs)
+                 :headers {"Content-Type"   "application/json"
+                           "X-TrackerToken" (env :pivotaltracker-api-key)}}
+        response @(client/post (tracker-post-url) options)]
+    (if (= 200 (:status response))
+      (json/read-str (:body response))
+      (throw (ex-info "Request failed" response)))))
 
 (defn -main
   [& args]

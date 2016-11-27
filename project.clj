1ad81ed45d95110f9334696e87848111199d84e5
apeckham <apeckham@gmail.com>
2016-11-27 14:20:59 -0800

    Add logging

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.2.0"]
-                 [environ "1.1.0"]]
+                 [environ "1.1.0"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :main ^:skip-aot hackerone-pivotaltracker.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -1,18 +1,21 @@
 (ns hackerone-pivotaltracker.core
   (:gen-class)
   (:require [org.httpkit.server :as server]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [clojure.tools.logging :as log]))
 
 (defn app [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
 
-(defn port-env []
+(defn get-port []
   (if-let [e (env :port)]
     (Integer/parseInt e)
     8080))
 
 (defn -main
   [& args]
-  (server/run-server app {:port (port-env)}))
+  (let [port (get-port)]
+    (log/info "Starting on port" port)
+    (server/run-server app {:port port})))

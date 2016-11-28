416703759deca7d30693183ca81096dd585702eb
apeckham <apeckham@gmail.com>
2016-11-28 09:22:14 -0800

    Add wrap-reload and start handling "/create"

--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -1,15 +1,20 @@
 (ns hackerone-pivotaltracker.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [org.httpkit.server :as server]
             [org.httpkit.client :as client]
             [clojure.data.json :as json]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.reload :refer [wrap-reload]]
             [environ.core :refer [env]]
             [clojure.tools.logging :as log]))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hackerone-pivotaltracker implementation to come"})
+(defroutes app
+           (GET "/create" request
+                (prn-str (:params request)))
+           (route/not-found "Not Found"))
 
 (defn stories-url []
   (format "https://www.pivotaltracker.com/services/v5/projects/%s/stories" (env :pivotaltracker-project-id)))
@@ -27,14 +32,14 @@
   (parse-or-throw @(client/post (stories-url) {:body    (json/write-str attrs)
                                                :headers tracker-request-headers})))
 
-#_(defn params->tracker [params]
-    (create-tracker-story {:name        (:title params)
-                           :description ""
-                           :story_type  "bug"
-                           :external_id "12345"}))
+(defn params->tracker [params]
+  (create-tracker-story {:name        (:title params)
+                         :description ""
+                         :story_type  "bug"
+                         :external_id "12345"}))
 
 (defn -main
   [& args]
   (let [port (Integer/parseInt (env :port "8080"))]
     (log/info "Starting on port" port)
-    (server/run-server app {:port port})))
+    (server/run-server (-> #'app wrap-keyword-params wrap-params wrap-reload) {:port port :max-line (* 1024 64)})))

8528e10a5f221ff9d473395b66d0358e0116e879
apeckham <apeckham@gmail.com>
2016-11-28 09:39:34 -0800

    implement /create and /view

--- src/hackerone_pivotaltracker/core.clj
+++ src/hackerone_pivotaltracker/core.clj
@@ -5,41 +5,50 @@
             [org.httpkit.server :as server]
             [org.httpkit.client :as client]
             [clojure.data.json :as json]
+            [ring.util.response :refer [redirect]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
             [ring.middleware.reload :refer [wrap-reload]]
             [environ.core :refer [env]]
             [clojure.tools.logging :as log]))
 
-(defroutes app
-           (GET "/create" request
-                (prn-str (:params request)))
-           (route/not-found "Not Found"))
+(defn story-url [id]
+  (format "https://www.pivotaltracker.com/n/projects/%s/stories/%s" (env :pivotaltracker-project-id) id))
 
-(defn stories-url []
+(def create-url
   (format "https://www.pivotaltracker.com/services/v5/projects/%s/stories" (env :pivotaltracker-project-id)))
 
-(defn parse-or-throw [response]
-  (if (= 200 (:status response))
-    (json/read-str (:body response))
+(def tracker-request-headers
+  {"Content-Type" "application/json" "X-TrackerToken" (env :pivotaltracker-api-key)})
+
+(def server-opts
+  {:port (Integer/parseInt (env :port "8080")) :max-line (* 1024 64)})
+
+(defn parse-response [{:keys [status body] :as response}]
+  (if (= 200 status)
+    (json/read-str body)
     (throw (ex-info "Request failed" response))))
 
-(def tracker-request-headers
-  {"Content-Type"   "application/json"
-   "X-TrackerToken" (env :pivotaltracker-api-key)})
-
 (defn create-tracker-story [attrs]
-  (parse-or-throw @(client/post (stories-url) {:body    (json/write-str attrs)
-                                               :headers tracker-request-headers})))
+  (->> {:headers tracker-request-headers :body (json/write-str attrs)}
+       (client/post create-url)
+       deref
+       parse-response))
 
-(defn params->tracker [params]
-  (create-tracker-story {:name        (:title params)
-                         :description ""
-                         :story_type  "bug"
-                         :external_id "12345"}))
+(defn params->tracker [{:keys [title details]}]
+  {:name        title
+   :description details
+   :labels      [{:name "hackerone"}]
+   :story_type  "bug"})
+
+(defroutes app
+           (GET "/create" request
+                (-> request :params params->tracker create-tracker-story (get "url") redirect))
+           (GET "/view" request
+                (-> request :params :id story-url redirect))
+           (route/not-found "Not Found"))
 
 (defn -main
   [& args]
-  (let [port (Integer/parseInt (env :port "8080"))]
-    (log/info "Starting on port" port)
-    (server/run-server (-> #'app wrap-keyword-params wrap-params wrap-reload) {:port port :max-line (* 1024 64)})))
+  (log/info server-opts)
+  (server/run-server (-> #'app wrap-keyword-params wrap-params wrap-reload) server-opts))

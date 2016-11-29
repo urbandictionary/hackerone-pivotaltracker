1e3088418a2c6218d703fc97ddb3c30371ac0ab3
apeckham <apeckham@gmail.com>
2016-11-28 17:22:41 -0800

    add some tests

--- test/hackerone_pivotaltracker/core_test.clj
+++ test/hackerone_pivotaltracker/core_test.clj
@@ -1,6 +1,14 @@
 (ns hackerone-pivotaltracker.core-test
   (:require [midje.sweet :refer :all]
+            [environ.core :refer [env]]
             [hackerone-pivotaltracker.core :refer :all]))
 
-(fact "is truthy"
-      1 => 1)
+(fact "from env var"
+      (tracker-api-key) => "foobar"
+      (provided
+        (env :pivotaltracker-api-key) => "foobar"))
+
+(fact "missing"
+      (tracker-api-key) => (throws "Tracker API key is not set")
+      (provided
+        (env :pivotaltracker-api-key) => nil))

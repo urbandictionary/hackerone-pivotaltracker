fcc6e93cfcad4e862268c9275e6349c955f13c19
apeckham <apeckham@gmail.com>
2016-11-28 17:24:27 -0800

    add tests

--- test/hackerone_pivotaltracker/core_test.clj
+++ test/hackerone_pivotaltracker/core_test.clj
@@ -3,12 +3,20 @@
             [environ.core :refer [env]]
             [hackerone-pivotaltracker.core :refer :all]))
 
-(fact "from env var"
-      (tracker-api-key) => "foobar"
-      (provided
-        (env :pivotaltracker-api-key) => "foobar"))
+(facts "tracker-api-key"
+       (fact "from env var"
+             (tracker-api-key) => "foobar"
+             (provided (env :pivotaltracker-api-key) => "foobar"))
 
-(fact "missing"
-      (tracker-api-key) => (throws "Tracker API key is not set")
-      (provided
-        (env :pivotaltracker-api-key) => nil))
+       (fact "missing"
+             (tracker-api-key) => (throws "Tracker API key is not set")
+             (provided (env :pivotaltracker-api-key) => nil)))
+
+(facts "tracker-project-id"
+       (fact "from env var"
+             (tracker-project-id) => 1234
+             (provided (env :pivotaltracker-project-id) => "1234"))
+
+       (fact "invalid"
+             (tracker-project-id) => (throws "For input string: \"asdf\"")
+             (provided (env :pivotaltracker-project-id) => "asdf")))
